1c39838df20c6fd0fd51f313e88b2c2e5fae2cf7
JRakhimov <rakhimov.5556@gmail.com>
2019-02-17 20:58:40 +0500

    feat: generate sections for all subjects

--- dist/index.js
+++ dist/index.js
@@ -6,10 +6,27 @@ const resource = new microcontroller_1.default();
 const freshmen = resource.getFreshmen();
 const subjects = resource.getSubjects();
 const rooms = resource.getRooms();
-const splittedGroups = methods_1.splitToGroups(freshmen.SOCIE);
-const splittedSections = methods_1.splitToSections(splittedGroups, subjects.freshman_1[0]);
+console.log('Subjects:');
 console.table(subjects.freshman_1);
-console.table(splittedGroups);
-console.table(splittedSections);
+console.log('Rooms:');
 console.table(rooms);
-console.log('Room: ', methods_1.findRoom(splittedSections[0], rooms));
+function genSchedule(students, subjects) {
+    const splittedGroups = methods_1.splitToGroups(students);
+    console.log('Groups:');
+    console.table(splittedGroups);
+    const schedule = [];
+    subjects.forEach(subject => {
+        const splittedSections = methods_1.splitToSections(splittedGroups, subject);
+        console.log(`Subject: ${subject.name}`);
+        console.table(splittedSections);
+        schedule.push({
+            subject: subject.name,
+            sections: splittedSections,
+            sectionsAmount: splittedSections.length
+        });
+    });
+    console.log('Schedule:');
+    console.table(schedule);
+    return schedule;
+}
+genSchedule(freshmen.SOCIE, subjects.freshman_1);
--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,42 @@
 import { splitToGroups, splitToSections, findRoom } from './helpers/methods';
 import Resource from './microcontroller';
+import { Subject, Subjects, Student } from './typings';
 
 const resource = new Resource();
 const freshmen = resource.getFreshmen();
 const subjects = resource.getSubjects();
 const rooms = resource.getRooms();
 
-const splittedGroups = splitToGroups(freshmen.SOCIE);
-const splittedSections = splitToSections(splittedGroups, subjects.freshman_1[0]); // Caution! Mutation sorts the groups array
-
+console.log('Subjects:');
 console.table(subjects.freshman_1);
-console.table(splittedGroups);
-console.table(splittedSections);
+console.log('Rooms:');
 console.table(rooms);
 
-console.log('Room: ', findRoom(splittedSections[0], rooms));
+// console.log('Room: ', findRoom(splittedSections[0], rooms));
+
+function genSchedule(students: Student[], subjects: Subject[]) {
+  const splittedGroups = splitToGroups(students);
+  console.log('Groups:');
+  console.table(splittedGroups);
+
+  const schedule = [];
+
+  subjects.forEach(subject => {
+    const splittedSections = splitToSections(splittedGroups, subject);
+    console.log(`Subject: ${subject.name}`);
+    console.table(splittedSections);
+
+    schedule.push({
+      subject: subject.name,
+      sections: splittedSections,
+      sectionsAmount: splittedSections.length
+    });
+  });
+
+  console.log('Schedule:');
+  console.table(schedule);
+
+  return schedule;
+}
+
+genSchedule(freshmen.SOCIE, subjects.freshman_1);
